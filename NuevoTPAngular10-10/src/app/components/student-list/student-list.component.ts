c62e55e6b4229bba8ad59a09fb33320e41799fca
AnaBeneitez <129800955+AnaBeneitez@users.noreply.github.com>
2023-10-11 10:26:18 -0300

    Update student-list.component.ts

--- NuevoTPAngular10-10/src/app/components/student-list/student-list.component.ts
+++ NuevoTPAngular10-10/src/app/components/student-list/student-list.component.ts
@@ -23,6 +23,11 @@ export class StudentListComponent implements OnInit {
   firstName2: string
   email2: string
 
+  dni3: string
+  lastName3: string
+  firstName3: string
+  email3: string
+
   constructor(private studentService: StudentService, private modalService: NgbModal) {}
 
   ngOnInit(): void {
@@ -76,6 +81,39 @@ export class StudentListComponent implements OnInit {
 
   view(view: any, s: StudentModule) {
     
-    
+    this.id2 = s.id
+    this.dni2 = s.dni
+    this.lastName2 = s.lastName
+    this.firstName2 = s.firstName
+    this.email2 = s.email
+
+    this.dni3 = s.dni
+    this.lastName3 = s.lastName
+    this.firstName3 = s.firstName
+    this.email3 = s.email
+
+    this.modalService.open(view).result.then(() => {
+      if(this.dni2.trim() !== '' && this.lastName2.trim() !== '' && this.firstName2.trim() !== '' && this.email2.trim() !== '' && 
+      (this.dni2.trim() !== this.dni3.trim() || this.lastName2.trim() !== this.lastName3.trim() || this.firstName2.trim() !== this.firstName3.trim() || this.email2.trim() !== this.email3.trim())) {
+        let student = new StudentModule()
+        student.id = this.id2
+        student.dni = this.dni2
+        student.lastName = this.lastName2
+        student.firstName = this.firstName2
+        student.email = this.email2
+        student.cohort = 0
+        student.status = 'activo'
+        student.gender = 'masculino'
+        student.adress = 'abc123'
+        student.phone = '000' 
+
+        this.studentService.update(student).subscribe(() => {
+          location.reload()
+        }, error => {
+          console.error(error)
+          alert('Error: ' + error.error.message)
+        })
+      }
+    })
   }
 }
